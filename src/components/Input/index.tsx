b958b95e1387875cec7be92b022a9f8f3416b671
Felipe Aguiar <felipe.aguiar@skipthedishes.ca>
2020-03-17 21:41:41 -0500

    move logic to hook

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {AddButton, TodoInput, InputWrapper, Wrapper} from './styles';
 import {FiChevronRight, FiPlus} from 'react-icons/fi';
 
@@ -6,12 +6,37 @@ type InputProps = {
   onAdd: (t: string) => void;
 }
 
-const Input = ({onAdd}: InputProps) => {
+const useTodo = (onAdd: (t: string) => void) => {
+  const [isEnabled, setEnabled] = useState(false);
   const [todo, setTodo] = useState('');
+
   const updateValue = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTodo(event.target.value);
   };
-  const addNewTodo = () => onAdd(todo);
+  const addNewTodo = () => {
+    if (isEnabled) {
+      onAdd(todo);
+      setTodo('');
+    }
+  };
+
+  useEffect(() => {
+    if (!todo.length) {
+      setEnabled(false);
+    } else {
+      setEnabled(true);
+    }
+  }, [todo]);
+
+  return {
+    addNewTodo,
+    todo,
+    updateValue,
+  };
+};
+
+const Input = ({onAdd}: InputProps) => {
+  const {addNewTodo, todo, updateValue} = useTodo(onAdd);
 
   return (
     <Wrapper>
