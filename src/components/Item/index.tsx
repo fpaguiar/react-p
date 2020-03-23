76475b5f995b597151d0cca002f2a35ea2d6319c
Felipe Aguiar <felipe.aguiar@skipthedishes.ca>
2020-03-22 19:19:11 -0500

    add icon when hovering item

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Wrapper, CheckBox, Content, Text} from './styles';
+import {FiGrid} from 'react-icons/fi';
 
 type ItemProps = {
   title: string;
 };
 
 const Item = ({title}: ItemProps) => {
+  const [isMouseHovering, setMouseHovering] = useState(false);
   return (
-    <Wrapper>
+    <Wrapper
+      onMouseEnter={() => setMouseHovering(true)}
+      onMouseLeave={() => setMouseHovering(false)}
+    >
       <Content>
         <CheckBox type="checkbox" />
         <Text>{title}</Text>
       </Content>
+      {isMouseHovering && <FiGrid stroke="grey" />}
     </Wrapper>
   );
 };
--- src/components/Item/styles.ts
+++ src/components/Item/styles.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
   margin: 10px 0;
   padding: 5px;
   &:hover {
